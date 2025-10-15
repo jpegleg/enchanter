d04a728d0a64095ecf48fed0c028b1d44c8b8b11
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-10-15 13:08:13 -0400

    Update version number from 0.1.1 to 0.1.2
    version printing update

--- src/main.rs
+++ src/main.rs
@@ -87,7 +87,7 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     }
     let input_file = &args[1];
     if input_file == "-v" {
-      println!("{{\"Version\": \"0.1.1\"}}");
+      println!("{{\"Version\": \"0.1.2\"}}");
       process::exit(0);
     }
     if args.len() != 4 {
